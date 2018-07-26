e6d657d6e47777ca18433cb0e921c58841e0ef76
lichangyu <943030904@qq.com>
2018-07-26 18:57:43 +0800

    增加静态模板

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const koa = require('koa')
 const app = new koa()
 
+const {normal} = require('./tpl')
+
 app.use(async (ctx, next) => {
-    ctx.body = 'hello world'
+  ctx.type = 'text/html; charset=utf-8'
+  ctx.body = normal
 })
 
 app.listen(2333)
